042d592c77034d45250ceb40381efa643ade85d0
Anjalii-2000 <anjalibhardwaj450@gmail.com>
2026-05-05 13:18:36 +0530

    how undefined , boolean null works

--- Js Chapter/Datatype.js
+++ Js Chapter/Datatype.js
@@ -28,3 +28,24 @@ console.log("type of b1", typeof b1);
 let c1 = 90n;
 console.log(c1);
 console.log(typeof c1);
+
+
+// String is a series of characters
+let d = " it is string";
+console.log(d);
+console.log(typeof d);
+
+// Boolean is a simple yes or no / true or false 
+let hasPassport = true;
+console.log(hasPassport);
+console.log(typeof hasPassport);
+
+//Undefine
+let e;
+console.log(e);
+console.log(typeof e);
+
+//Null 
+let fg = null;
+console.log(fg);     
+console.log(typeof fg);   // old bug its datatype is object 
