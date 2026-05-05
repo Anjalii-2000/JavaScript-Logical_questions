5d6b3ea336daf30e29a5d1a7979fa790d101be3b
Anjalii-2000 <anjalibhardwaj450@gmail.com>
2026-05-05 14:07:21 +0530

    how many times the element comes

---
+++ array-frequency.js
@@ -0,0 +1,16 @@
+let a = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+
+function countElement(arr) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];  // FIX HERE
+        if (obj[ele]) {
+            obj[ele] = obj[ele] + 1;
+        } else {
+            obj[ele] = 1;
+        }
+    }
+    return obj;
+}
+
+console.log(countElement(a));
