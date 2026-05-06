63dbf3162dd991ddadc007092df48b7cde24e7af
Anjalii-2000 <anjalibhardwaj450@gmail.com>
2026-05-06 14:47:35 +0530

    task done by using recursion and for loop

--- recursion.js
+++ recursion.js
@@ -1,19 +1,19 @@
-// function coding(num) {
-//     if (num === 0) {
-//         console.log("coding has been done");
-//         return;
-//     } else {
-//         console.log(" i am doing coding");
-//         coding(num - 1);
-//     }
-// }
-// coding(6);
-
-
-function abc() {
-    if(a === 1){
-        console.log(“waiting to get equal value”);
-    }
-    abc();
+// task is by for loop 
+function result(x){
+  let resultt = 1;
+  for(let i=1; i<= x; i++){
+    resultt = resultt * i;
+  }
+  return resultt
 }
-abc(7);
+
+console.log(result(5));  // factorial - 120 
+
+
+// now task is done by recursion
+function result(x){
+  if(x==0) return 1;
+  return x * result(x-1);
+}
+
+console.log(result(3)); // 6 we have to return otherwise it will give  type error 
