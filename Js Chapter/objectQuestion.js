93ecbd94d9ec5600197152c023124dee06c66abe
Anjalii-2000 <anjalibhardwaj450@gmail.com>
2026-05-08 16:45:38 +0530

    practising of question

--- Js Chapter/objectQuestion.js
+++ Js Chapter/objectQuestion.js
@@ -5,18 +5,6 @@ const obj = [
 ];
 
 let result = [];
-for (let i = 0; i < obj.length; i++) {
-let bookName = Object.keys(obj[i])[0];
-console.log(bookName);
-}
-
-const obj = [
-    { Book1: ["auth1", "auth2"] },
-    { Book2: ["auth3", "auth4", "auth5"] },
-    { Book3: ["auth6", "auth1", "auth7"] }
-];
-
-let result = [];
 
 for (let i = 0; i < obj.length; i++) {
 
